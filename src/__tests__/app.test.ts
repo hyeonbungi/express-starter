1e887ae78c31e2fc271c7537c18cef270046cf02
hyeonbungi <kimhyeonbung@gmail.com>
2025-04-12 23:37:30 +0900

    테스트: GET / 엔드포인트의 응답 메시지를 'Hello, Express!'로 수정

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -4,6 +4,6 @@ import app from '../app';
 describe('GET /', () => {
   it('should return Hello, World!', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('Hello, world!');
+    expect(res.text).toBe('Hello, Express!');
   });
 });
